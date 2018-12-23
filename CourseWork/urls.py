49876dc2f9d82e208df1c28db767c6045086da79
ivanovds <danil.98and@gmail.com>
2018-12-23 20:42:27 +0200

    commit

--- CourseWork/urls.py
+++ CourseWork/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
-# from myapp.views import index
+
+from accounts.views import (login_view, register_view, logout_view)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('myapp.urls')),
+    url(r'^login/', login_view, name='login')
 ]
