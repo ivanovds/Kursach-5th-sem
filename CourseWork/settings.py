e8fcc515a89b5ab3e00f822f190343ba310f5db5
ivanovds <danil.98and@gmail.com>
2018-12-24 13:08:31 +0200

    user1

--- CourseWork/settings.py
+++ CourseWork/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kursach',
-        'USER': 'postgres',
-        'PASSWORD': '5133',
+        'USER': 'user',
+        'PASSWORD': 'pas123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
