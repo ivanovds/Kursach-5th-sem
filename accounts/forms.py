06177667d45abd2e35bc2b31c32e7ba9d41ec3f0
ivanovds <danil.98and@gmail.com>
2018-12-25 13:15:47 +0200

    register signIn signUp

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,12 +4,15 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(max_length=150)
-    email = forms.EmailField(max_length=254, help_text='')
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'placeholder': 'Username'}))
+    first_name = forms.CharField(max_length=30, required=False, widget=forms.TextInput(
+        attrs={'placeholder': 'Real Name(optional)'}))
+    email = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', )
-
+        fields = ('username', 'first_name', 'email', 'password1', 'password2', )
 
 
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+#
+# class Order(models.Model):
+#     course = models.CharField(max_length=128)
+#     username = models.CharField(max_length=128)
+
