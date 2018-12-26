d1e92b6e6c61810a58a7bd8dbbb28c7fdc0b9b65
ivanovds <danil.98and@gmail.com>
2018-12-27 00:20:46 +0200

    commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import MyUser
 
 
 class UserRegisterForm(UserCreationForm):
@@ -17,10 +16,4 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ('username', 'first_name', 'email', 'password1', 'password2', )
 
-
-class MyUserForm(forms.ModelForm):
-
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'email', 'password')
 
--- accounts/views.py
+++ accounts/views.py
@@ -7,22 +7,14 @@ from django.contrib.auth import (
 )
 from django.shortcuts import render, redirect
 
-from .forms import UserRegisterForm, MyUserForm
+from .forms import UserRegisterForm
 
 
 def register_view(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST or None)
-        # my_form = MyUserForm()
         if form.is_valid():
             form.save()
-
-            # my_form.username = form.cleaned_data.get('username')
-            # my_form.first_name = form.cleaned_data.get('first_name')
-            # my_form.email = form.cleaned_data.get('email')
-            # my_form.first_name = form.cleaned_data.get('password1')
-            # my_form.save()
-
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
